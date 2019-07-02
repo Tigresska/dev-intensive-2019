8239e90ae60593d37a2b3c28b96ef66e086f4a71
Tigresska <tigresska@gmail.com>
2019-07-02 16:34:20 +0300

    hometask_2 complete Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -19,6 +19,43 @@ data class User(
     val isOnline: Boolean = false
 
 ) {
+    /*
+    Реализуй паттерн Builder для класса User.
+    User.Builder().id(s)
+    .firstName(s)
+    .lastName(s)
+    .avatar(s)
+    .rating(n)
+    .respect(n)
+    .lastVisit(d)
+    .isOnline(b)
+    .build() должен вернуть объект User
+    */
+    class Builder {
+        var id: String = "0"
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = Date()
+        var isOnline: Boolean = false
+
+        fun id (id: String) = apply { this.id  = id }
+        fun firstName (firstName: String?) = apply { this.firstName  = firstName }
+        fun lastName (lastName: String?) = apply { this.lastName  = lastName }
+        fun avatar (avatar: String?) = apply { this.avatar  = avatar }
+        fun rating (rating: Int) = apply { this.rating  = rating }
+        fun respect (respect: Int) = apply { this.respect  = respect }
+        fun lastVisit (lastVisit: Date?) = apply { this.lastVisit  = lastVisit }
+        fun isOnline (isOnline: Boolean) = apply { this.isOnline  = isOnline }
+        fun build() = User(this)
+
+
+
+
+
+    }
 //    var introBit :String
 
     constructor(id: String, firstName: String?, lastName: String?) : this(
@@ -28,7 +65,19 @@ data class User(
         avatar = null
     )
 
-    constructor(id: String) : this(id, "John", "Doe")
+//    constructor(id: Builder) : this(id, "John", "Doe")
+    constructor(builder: Builder) : this(
+    builder.id,
+    builder.firstName,
+    builder.lastName,
+    builder.avatar,
+    builder.rating,
+    builder.respect,
+    builder.lastVisit,
+    builder.isOnline
+
+
+)
 
     init {
 //        introBit = getIntro()
